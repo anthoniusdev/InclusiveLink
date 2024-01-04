44c88eedd476ea82adfc7c1b1ed69eae9786688e
Abimael Lima de Olivieri <abimael034@gmail.com>
2024-01-04 13:27:48 -0300

    04/01/24-13:27

--- src/main/java/model/SeguidorComunidade.java
+++ src/main/java/model/SeguidorComunidade.java
@@ -1,7 +1,7 @@
 package model;
 
 public class SeguidorComunidade extends Membro{
-    public SeguidorComunidade(int idPessoa, String nome, String dataNascimento, String email, String senha) {
-        super(idPessoa, nome, dataNascimento, email, senha);
+    public SeguidorComunidade(Membro  membroParaSeguidor){
+        super(membroParaSeguidor.getIdPessoa(), membroParaSeguidor.getNome(), membroParaSeguidor.getDataNascimento(), membroParaSeguidor.getEmail(), membroParaSeguidor.getSenha());
     }
 }
