b0ee1ea6c4accb91266806af49bc08b2bc836c6f
Anthonius <anthoniusmiguel@gmail.com>
2024-01-04 13:03:51 -0300

    Sugestão de Alteração

--- src/main/java/model/ParticipanteComunidade.java
+++ src/main/java/model/ParticipanteComunidade.java
@@ -1,7 +1,13 @@
 package model;
 
 public class ParticipanteComunidade extends SeguidorComunidade{
+    /*
     public ParticipanteComunidade(int idPessoa, String nome, String dataNascimento, String email, String senha) {
         super(idPessoa, nome, dataNascimento, email, senha);
+    }*/
+
+    public ParticipanteComunidade(SeguidorComunidade  seguidorComunidade){
+        super(seguidorComunidade.getIdPessoa(), seguidorComunidade.getNome(), seguidorComunidade.getDataNascimento(), seguidorComunidade.getEmail(), seguidorComunidade.getSenha());
+
     }
 }
