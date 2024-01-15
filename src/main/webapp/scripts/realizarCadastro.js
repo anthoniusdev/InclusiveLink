7da3cd8791a1f09e8723dd131c5254a80acf096d
Anthonius <anthoniusmiguel@gmail.com>
2024-01-15 12:51:39 -0300

    Adicionando a validação de nome no JS

--- src/main/webapp/scripts/realizarCadastro.js
+++ src/main/webapp/scripts/realizarCadastro.js
@@ -1,4 +1,91 @@
-function realizarCadastro() {
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Script carregado com sucesso.");
+    const form = document.getElementById("formCadastro");
+    const nome = document.getElementById("nome");
+    const sobrenome = document.getElementById("sobrenome");
+    const nomeUsuario = document.getElementById("nome_usuario");
+    const email = document.getElementById("email");
+    const senha = document.getElementById("senha");
+    const senhaRepetida = document.getElementById("senha_repetida");
+    const dia = document.getElementById("dia");
+    const mes = document.getElementById("mes");
+    const ano = document.getElementById("ano");
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        checkInput();
+    })
+
+    function checkInput() {
+        const nomeValue = nome.value;
+        const sobrenomeValue = sobrenome.value;
+        const nomeUsuarioValue = nomeUsuario.value;
+        const emailValue = email.value;
+        const senhaValue = senha.value;
+        const senhaRepetidaValue = senhaRepetida.value;
+        // Checando se o nome está vazio
+        if (nomeValue.trim() === "") {
+            // Mostrar o erro
+            // adicionar a classe error
+            errorValidation(nome, "Preencha esse campo");
+        } else {
+            // adicionar a classe de sucesso
+            succesValidation(nome);
+        }
+    }
+
+    function errorValidation(input, message) {
+        const formControl = input.parentElement;
+        if (formControl) {
+            const small = formControl.parentElement.querySelector('small');
+            if (small) {
+                const i = formControl.parentElement.querySelector('i');
+                if (i) {
+                    const img = i.querySelector('img');
+                    if (img) {
+                        i.style.visibility = "visible";
+                        img.src = "images/error-icon.svg";
+                        img.alt = "Imagem de erro!";
+                        img.style.visibility = "visible";
+                        small.innerText = message;
+                        formControl.className = 'form-control error';
+                        small.style.visibility = "visible";
+                        small.style.color = "#782121";
+                    } else {
+                        console.error("Elemento img não encontrado");
+                    }
+                } else {
+                    console.error("Elemento i não encontrado");
+                }
+            }else {
+                console.log("ELEMENTO SMALL NÃO ENCONTRADO");
+            }
+        }else{
+            console.log("FORM CONTROL NÃO ENCONTRADO");
+        }
+    }
+    function succesValidation(input){
+        const formControl = input.parentElement;
+        if (formControl){
+            const i = formControl.parentElement.querySelector('i');
+            if (i){
+                const img = i.querySelector('img');
+                if (img){
+                    i.style.visibility = "visible";
+                    img.src = "images/success-icon.png";
+                    formControl.className = "form-control success";
+                }else {
+                    console.log("ELEMENTO IMG NAO ENCONTRADO")
+                }
+            }else {
+                console.log("ELEMENTO I NAO ENCONTRADO LINHA 72");
+            }
+        }else {
+            console.log("FORM CONTROL NAO ENCONTRADO LINHA 70");
+        }
+    }
+});
+/*function realizarCadastro() {
     let formulario = frmCadastro;
     let nome = formulario["nome"];
     let sobrenome = formulario["sobrenome"];
@@ -46,4 +133,4 @@ function corrigirStringEValidar(str) {
     
     // Testa se a string corrigida atende ao padrão da expressão regular
     return regex.test(stringCorrigida) ? stringCorrigida : false;
-}
+}*/
