5066f4d952c956bee0ceab39a14d9f9e3c77f0f3
Igor Souza Santos <igorsouza1771marketing@gmail.com>
2024-02-20 08:43:48 -0300

    20/02/2024-08:42

--- src/main/java/controller/ModeradorComunidadeController.java
+++ src/main/java/controller/ModeradorComunidadeController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/adicionarModerador", "/excluirComunidade", "excluirParticipante"})
+@WebServlet(urlPatterns = {"/adicionarModerador", "/excluirComunidade", "/excluirParticipante"})
 public class ModeradorComunidadeController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
